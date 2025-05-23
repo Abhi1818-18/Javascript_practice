c5aa4e5bbcd85c4d6381f89a8055677573a43854
Abhi1818-18 <abhishekgurav1818@gmail.com>
2025-05-23 08:32:59 +0530

    added

---
+++ operators.js
@@ -0,0 +1,34 @@
+let addition = 3 + 2;
+console.log(addition);
+
+let subtraction = 5 - 2;
+let multiplication = 4 * 2;
+let division = 8 / 2;
+let modulus = 5 % 2;
+console.log(modulus);
+
+let exponentiation = 2 ** 3;
+console.log(exponentiation);
+
+
+let myScore = 234;
+myScore+=23;
+console.log(myScore);
+
+myScore--;
+console.log(myScore);
+
+//comparision operators
+let num1 = 34; let num2 = 33 ; let num3 = 34;
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1>num2);
+
+//logical operators
+let isLoggedIn = true;
+let isAdmin = false;
+console.log(isLoggedIn && isAdmin);
+isAdmin=true;
+console.log(isLoggedIn && isAdmin);
+console.log(isLoggedIn || isAdmin);
+console.log(!isLoggedIn);
